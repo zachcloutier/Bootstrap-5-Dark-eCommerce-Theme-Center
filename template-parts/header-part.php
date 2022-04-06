17b190932d255b5551dd734f6326ac6466203941
Zach Cloutier <zach@cclcontrollers.com>
2022-04-06 10:46:17 -0500

    set logo height

--- template-parts/header-part.php
+++ template-parts/header-part.php
@@ -42,7 +42,7 @@
                         $custom_logo_id = get_theme_mod( 'custom_logo' );
                         $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
                     ?>
-                    <img src="<?php echo $image[0]; ?>" class="custom-logo" alt="" loading="lazy" />
+                    <img src="<?php echo $image[0]; ?>" class="custom-logo" alt="" loading="lazy" height="630px" />
                 </a>
             </div>
             
